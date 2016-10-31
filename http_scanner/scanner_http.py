650186b7e0d8a3e10f67e3594df477a9afc190fc
Kamil <gryxon@gmail.com>
2016-10-31 14:15:41 +0100

    Create first poc of application archs

--- http_scanner/scanner_http.py
+++ http_scanner/scanner_http.py
@@ -1,2 +1,26 @@
+import json
+
 class ScannerHttp(object):
-    pass
+"""
+    Main scanner class.
+"""
+
+    ScannerHttp(self):
+    """
+        Constructor of out scanner.
+        Currently there is only one field in class (protected).
+        The field with result is dictonary.
+    """
+        self._result = {}
+
+    get_result(self):
+    """
+        Getter of result field
+    """
+        return self._result
+
+    get_result_json(self):
+    """
+        Getter of json version of result field.
+    """
+        return json.dumps(self._result)
--- scanner_http_runner.py
+++ scanner_http_runner.py
@@ -1,1 +1,15 @@
+import argparse
 
+def get_parsed_args():
+    parser = argparse.ArgumentParser("Argsparser for Http scanner.")
+    parser.add_argument("--url", help="Url of server", default="localhost",
+                        type=str)
+    parser.add_argument("-o", action="store_true",
+                        help="Scan of version http server and OS of machine")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_parsed_args()
+    print args.o
+    print args.url
