d61ed676282898b752ee373402e5e6b2a8fb170d
root <root@localhost.localdomain>
2017-01-06 01:23:30 +0100

    conf_erros seems ok

--- http_scanner/conf_errors.py
+++ http_scanner/conf_errors.py
@@ -1,40 +1,58 @@
-import xml.etree.ElementTree
-import re
+class conf_errors(object):
 
-a = ""
-b = 0
-in_tag = False
-c = []
-f = open('/etc/apache2/apache2.conf', 'r')
-for line in f.readlines():
-    if b%2 == 0:
-        if line[0] == ('<'):
-            a = a + line
-            b = b+1
-    else:
-        a = a + line
-        if line[0] == ('<'):
-            b = b+1
-
-f.close()
-
-for i in a.splitlines():
-
-    if "<Directory" in i:
-        in_tag = True
-        continue
-
-    if "</Directory" in i:
-        in_tag = False
-        continue
-
-    if in_tag:
-        c.append(i)
-
-if "Options FollowSymLinks" in c:
-    print 'dupa'
+    def __init__(self):
+        self.a = ""
+        self.b = 0
+        self.in_tag = False
+        self.c = []
+        self.result = {}
+        self.id_mod = 'conf_err'
 
 
+    def find_errors(self):
+        f = open('/etc/apache2/apache2.conf', 'r')
+        for line in f.readlines():
+            if self.b%2 == 0:
+                if line[0] == ('<'):
+                    self.a = self.a + line
+                    self.b = self.b+1
+            else:
+                self.a = self.a + line
+                if line[0] == ('<'):
+                    self.b = self.b+1
+        f.close()
 
-#print a.splitlines()[1]
-#e = xml.etree.ElementTree.fromstring(a)
+        for i in self.a.splitlines():
+
+            if "<Directory" in i:
+                in_tag = True
+                continue
+
+            if "</Directory" in i:
+                in_tag = False
+                continue
+
+            if in_tag:
+                s = i.lower()
+                self.c.append(s)
+
+        for i in self.c:
+            if ("dav on" or "allow from all") in i:
+                self.result = "Dangerous server configuration!!!"
+            else:
+                self.result = "Configuration seems fine"
+
+    def get_result(self):
+        return self.result
+
+
+if __name__ == '__main__':
+
+    module = conf_errors()
+    module.find_errors()
+    print module.get_result()
+
+
+
+
+
