bd9b7d3e36dc412ee15eac87dd2191221225c26c
Vivaq <viva995@gmail.com>
2017-01-06 18:44:17 +0100

    moje zmiany przerzucone

--- http_scanner/white-box-modules/permission_check.py
+++ http_scanner/white-box-modules/permission_check.py
@@ -2,19 +2,17 @@ import os
 
 
 class PermissionCheck(object):
-    def __init__(self):
+    def __init__(self, server):
         self.result= {}
         self.id_mod = 'permission'
-        self.paths = ['/etc/apache2/apache2.conf',
-                      '/etc/apache2/sites-available/000-default.conf',
-                      '/etc/apache2/sites-available/default-ssl.conf']
+        self.paths = ['/etc/' + server + '/' + server + '.conf', '/etc/' + server + '/sites-enabled/'] \
+            if server.lower() != 'windows' else ['C:\\']
     
     def check_all(self):
         for p in self.paths:
             self.check_permission(p)
 
     def check_permission(self, path):
-
         if os.path.isfile(path):
             st = os.stat(path)
             print oct(st.st_mode)[-3:][2]
@@ -22,14 +20,17 @@ class PermissionCheck(object):
                 self.result[path] = 'Niebezpieczne uprawnienia dostepu!!!'
             else:
                 self.result[path] = "Wszystko spoko"
-
+        elif os.path.isdir(path):
+            os.access(path, os.W_OK)
+            if os.R_OK == 4:
+                self.result[path] = 'Niebezpieczne uprawnienia dostepu!!!'
         else:
-            self.result[path] = "Nie ma takiego pliku"
+            self.result[path] = "Nie ma takiego pliku lub folderu"
 
     def get_result(self):
         return self.result
 
 if __name__ == '__main__':
-    module = PermissionCheck()
+    module = PermissionCheck('nginx')
     module.check_all()
     print module.get_result()
